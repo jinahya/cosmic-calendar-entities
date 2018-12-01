5e0831edeedbc001d5f153e160ec86b58610a39a
Jin Kwon <onacit@gmail.com>
2018-12-01 22:48:13 +0900

    updated

---
+++ src/main/java/com/github/jinahya/cosmic/calendar/persistence/BaseEntity.java
@@ -0,0 +1,7 @@
+package com.github.jinahya.cosmic.calendar.persistence;
+
+import java.io.Serializable;
+
+//@MappedSuperclass
+public class BaseEntity implements Serializable {
+}
---
+++ src/main/java/com/github/jinahya/cosmic/calendar/persistence/Event.java
@@ -0,0 +1,9 @@
+package com.github.jinahya.cosmic.calendar.persistence;
+
+public class Event extends BaseEntity {
+
+    // -----------------------------------------------------------------------------------------------------------------
+    public static final String TABLE_NAME = "EVENT";
+
+    // -----------------------------------------------------------------------------------------------------------------
+}
